b94f0f7387e07d6783b91d5685c1673eedf7e759
sebastiantuyu <alextpineiro@gmail.com>
2024-04-26 11:47:06 -0600

    [ci skip]: chore: simple http200

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    println!("[rapi]: ⚡️ Server started on port 4221 ⚡️");
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                _ = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
+                _ = stream.flush();
             }
             Err(e) => {
                 println!("error: {}", e);
