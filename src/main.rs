4e9def0e4dd6823c6a640f6450b6b360327edc02
sebastiantuyu <alextpineiro@gmail.com>
2024-04-26 15:51:45 -0600

    chore: remove comments

--- src/main.rs
+++ src/main.rs
@@ -13,28 +13,6 @@ use response::Response;
 use crate::server::Server;
 
 fn main() {
-    // let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    // println!("[rapi]: ⚡️ Server started on port 4221 ⚡️");
-
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(mut stream) => {
-    //             let mut buff = [0; 255];
-    //             let size_buff = stream.read(&mut buff).unwrap();
-    //             let request = parse_html(&buff[..size_buff].to_vec());
-    //             match request {
-    //                 Some(req) => {
-    //                     _ = dispatch(req, Response::new(stream));
-    //                 }
-    //                 _ => {
-    //                 }
-    //             }
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
     let mut app = Server::new();
     app.config("127.0.0.1:4221".to_string());
     app.start(|mut stream| {
